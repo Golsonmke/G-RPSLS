07c99c3d3b9edc53fc847ad889de12976618238b
unknown <golsonmke@gmail.com>
2020-02-11 13:38:25 -0600

    init

---
+++ RPSLSv.1/Game.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RPSLSv._1
+{
+    class Game
+    {
+    }
+}
---
+++ RPSLSv.1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace RPSLSv._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
